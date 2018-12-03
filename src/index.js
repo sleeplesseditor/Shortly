e37df2c633a4266aad52b4ef42d5d89c8a7d23ac
sleeplesseditor <josh@bryson.me.uk>
2018-12-03 16:12:54 +0000

    First component setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const serviceId = require('./config/keys').serviceID;
+const client = new ApolloClient({
+    link: new HttpLink(`https://api.graph.cool/simple/v1/${serviceId}`),
+    cache: new InMemoryCache(),
+});
+const withApolloProvider = Comp => (
+    <ApolloProvider client={client}>{Comp}</ApolloProvider>
+  );
+
+ReactDOM.render(
+    withApolloProvider(<App />), 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
