ca6f4a87523e9e9fc3c5e56277fcc3dba6f503ca
sleeplesseditor <josh@bryson.me.uk>
2018-12-03 18:55:02 +0000

    CreateShortLink component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import LinkList from './components/LinkList';
+import CreateShortLink from './components/CreateShortLink';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <div>
+        <div>
+          <h2>All Links</h2>
+          <LinkList />
+        </div>
+        <div>
+          <h2>Create a Short Link</h2>
+          <CreateShortLink />
+        </div>
+      </div>
     );
   }
 }
