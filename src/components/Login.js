0c724b2afa617c12e390361b800bebfc9a02d847
sleeplesseditor <josh@bryson.me.uk>
2018-12-04 00:34:04 +0000

    Login and Signup component setup

---
+++ src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    login = async () => {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Login to Shortly</h2>
+                <input
+                    id="email"
+                    type="email"
+                    value={this.state.email}
+                    placeholder="Email Address"
+                    onChange={e => 
+                        this.setState({ email: e.target.value })
+                    }
+                />
+                <br />
+                <input
+                    id="password"
+                    type="password"
+                    value={this.state.password}
+                    placeholder="Password"
+                    onChange={e => 
+                        this.setState({ password: e.target.value })
+                    }
+                />
+                <br />
+                <button onClick={() => this.login()}>Log In</button>
+            </div>
+        );
+    }
+}
+
+export default Login;
